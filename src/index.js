79a44b8802a157dc5780c571413a9f2534b95a42
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-02-28 00:07:02 +0300

    feat: add functions to solve the task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,79 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const stack = [];
+
+  let stackTop;
   
+  const isStackEmpty = (stack) => {
+    if (stack.length === 0) {
+      return true;
+    }
+  }
+
+  const isStrElementOpenBracket = (strElement) => {
+    let result;
+
+    const hasOpenBracket = (element) => {
+      if (strElement === element[0]) {
+        result = true;
+      }
+    }
+  
+    bracketsConfig.forEach(hasOpenBracket);
+  
+    return result;
+  }
+
+  const isFirstBracketClosing = (strElement) => {
+    let result;
+
+    const hasClosingBracket = (element) => {
+      if (strElement === element[1]) {
+        result = true;
+      }
+    }
+  
+    bracketsConfig.forEach(hasClosingBracket);
+    
+    if (result === true && isStackEmpty(stack) === true) {
+      return true;
+    }
+  }
+  
+  const compareClosingAndOpenBracket = (strElement) => {
+    let result;
+
+    const getPairBracket = (element) => {
+      if (strElement === element[0]) {
+        result = element[1];
+      }
+    }
+  
+    bracketsConfig.forEach(getPairBracket);
+  
+    return result;
+  }
+
+  
+  
+  [...str].forEach(bracket => {
+    stackTop = stack.at(-1);
+
+    if (isFirstBracketClosing(bracket)) {
+      return false;
+    }
+    
+    if (isStrElementOpenBracket(bracket)) {
+      stack.push(bracket);
+    } else if(stackTop === compareClosingAndOpenBracket(bracket)) {
+      stack.pop();
+    }
+
+  })
+  
+  if (isStackEmpty) {
+    return true;
+  } else {
+    return false;
+  }
 }
